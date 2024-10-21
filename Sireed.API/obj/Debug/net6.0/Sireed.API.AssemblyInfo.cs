1a2f57a602000c0e7b388f7df7c427cb55c004b0
ACHRAF DAHDAH <83411013+achhraf87@users.noreply.github.com>
2024-10-21 23:46:40 +0200

    Ok

--- Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
+++ Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e5d08d4345017d02d2e384156cd37b9f7381b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7be3834e3c3537422035cad3a2f9199c94a4c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

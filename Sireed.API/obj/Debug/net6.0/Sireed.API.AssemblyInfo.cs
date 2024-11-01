0318b14680fd16b27d7d43da5fbcf89940db5d73
achrafDotNet <achrafdahdah.net@gmail.com>
2024-11-01 18:30:40 +0100

    OK

--- Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
+++ Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a89534cce9f6f505e266fcb6a0dc6e60a464b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2a22e1f6a64fbab5c52c92b8476866eab700b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

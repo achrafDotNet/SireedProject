a6cd8ca3546c7dc99336c827da848829202d1ae9
achrafDotNet <achrafdahdah.net@gmail.com>
2024-11-07 18:00:05 +0100

    ok
    + Fix NavBar

--- Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
+++ Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fb72af91addd8119a899f4646d7860cf3d0190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acae12ad43986f6c76e986eccac6b88f4dea470e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

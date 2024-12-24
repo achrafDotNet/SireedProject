13f845610abf6fc6dd89dc96ba8fedb2b8ebe2f9
achrafDotNet <achrafdahdah.net@gmail.com>
2024-12-24 11:44:26 +0100

    SYSTEME D'INFORMATION REGIONAL DE L'ENVIRONNEMENT ET DU DEVELOPPEMENT DURABLE
    SYSTEME D'INFORMATION REGIONAL DE L'ENVIRONNEMENT ET DU DEVELOPPEMENT DURABLE

--- Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
+++ Sireed.API/obj/Debug/net6.0/Sireed.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1d58590ce4cbef1f83f17f071f43477b96a0f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ee1e733ef55f0b50dbd16a836ba4c4867185e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sireed.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
